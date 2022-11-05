b0e6d05e81de80610f5be6fbb509369a962359d9
BuNaTin <info444@ya.ru>
2022-11-05 20:42:50 +0300

    Add server implemented proto file iface

--- server/main.cpp
+++ server/main.cpp
@@ -1,10 +1,49 @@
-#include <sender.grpc.pb.h>
+#include <FileReciever/Server.h>
 
-#include <iostream>
+#include <Logger.h>
+#include <argparse/argparse.hpp>
+#include <types.h>
+
+#define ARG_ADDRESS "--address"
+#define ARG_ADDRESS_SHORT "-a"
+
+#define ARG_PORT "--port"
+#define ARG_PORT_SHORT "-p"
+
+#define ARG_HELP "--help"
+
+void buildArgs(argparse::ArgumentParser &parser) {
+    parser.add_argument(ARG_ADDRESS, ARG_ADDRESS_SHORT)
+            .help("where should server bind on")
+            .default_value(std::string("0.0.0.0"));
+    parser.add_argument(ARG_PORT, ARG_PORT_SHORT)
+            .help("server bind port")
+            .default_value(9090)
+            .scan<'i', int>();
+}
 
 int main(int argc, char *argv[]) {
-    
-    std::cout << "Check cmake includes" << std::endl; 
+    checkBytes();
+    argparse::ArgumentParser arg_parser("File reciever server");
+    buildArgs(arg_parser);
+    try {
+        arg_parser.parse_args(argc, argv);
+    } catch (std::runtime_error &err) {
+        LOGE("{} while parsing arguments", err.what());
+        return 1;
+    } catch (std::logic_error &err) {
+        LOGE("{} in logic while parsing arguments", err.what());
+        return 1;
+    }
+
+    if(arg_parser.is_used(ARG_HELP)) {
+        std::cout << arg_parser.help().str() << std::endl;
+        return 0;
+    }
+
+    std::string bind_address = arg_parser.get<std::string>(ARG_ADDRESS);
+    u16 bind_port = arg_parser.get<int>(ARG_PORT);
+    FileReciever::RunServer(bind_address, bind_port);
 
     return 0;
 }
